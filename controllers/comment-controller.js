a63e6dedd074d7596328a24b5d58a073166fffa9
Mpergolatti <Mpergolatti@gmail.com>
2022-11-27 12:17:52 -0500

    added reply functionality

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,6 +21,34 @@ const commentController = {
       })
       .catch(err => res.json(err));
     },
+
+    // Update Reply on Pizza
+    addReply({ params, body }, res) {
+      Comment.findOneAndUpdate(
+          { _id: params.commentId },
+          { $push: { replies: body } },
+          { new: true }
+        )
+          .then(dbPizzaData => {
+            if(!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => res.json(err));
+    },
+
+    // Remove Reply
+    removeReply({ params }, res) {
+      Comment.findOneAndUpdate(
+          { _id: params.commentId },
+          { $pull: { replies: { replyId: params.replyId } } },
+          { new: true}
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
+    },
   
   // Remove Comment
   removeComment({ params }, res) {
